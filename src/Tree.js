2978e39cfe27acefef5afe14341b4d4ad1da48ae
NadyaCodes <nadyacorscadden@gmail.com>
2022-12-23 23:27:43 -0800

    add christmas tree

---
+++ src/Tree.js
@@ -0,0 +1,10 @@
+import React from "react";
+import tree from "./christmas-tree.png";
+
+export default function Tree() {
+  return (
+    <div>
+      <img src={tree} className="tree" alt="large christmas tree" />
+    </div>
+  );
+}
