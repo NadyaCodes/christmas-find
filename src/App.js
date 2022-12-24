43adcfeb58d4dc52f9abf8ed8ee5d50cdb58fd4f
NadyaCodes <nadyacorscadden@gmail.com>
2022-12-23 23:38:52 -0800

    create container that hides behind star

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import "./App.css";
 import Tree from "./Tree";
+import Grid from "./Grid";
 
 function App() {
   return (
     <div className="App">
       <Tree />
+      <Grid />
     </div>
   );
 }
