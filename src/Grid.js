9b7977e44d91b53219c1a5fcb1b7d1ac690bc958
NadyaCodes <nadyacorscadden@gmail.com>
2022-12-24 00:26:25 -0800

    place pot hiding spot

--- src/Grid.js
+++ src/Grid.js
@@ -3,16 +3,33 @@ import frog from "./frog-face-cute.png";
 import { useState } from "react";
 
 export default function Grid() {
-  const [visible, setVisible] = useState(false);
+  const [visible, setVisible] = useState({ star: false, pot: false });
   return (
     <div className="grid">
-      <div className="box star-hide" onClick={() => setVisible(true)}></div>
-      <div>
-        <img
-          src={frog}
-          alt="cute frog with bow"
-          className={visible === true ? "frog visible" : "hidden"}
-        />
+      <div className="hiding-container">
+        <div
+          className="box star-hide"
+          onClick={() => setVisible({ ...visible, star: true })}
+        ></div>
+        <div>
+          <img
+            src={frog}
+            alt="cute frog with bow"
+            className={visible.star === true ? "frog visible" : "hidden"}
+          />
+        </div>
+
+        <div
+          className="box pot-hide"
+          onClick={() => setVisible({ ...visible, pot: true })}
+        ></div>
+        <div>
+          <img
+            src={frog}
+            alt="cute frog with bow"
+            className={visible.pot === true ? "frog visible" : "hidden"}
+          />
+        </div>
       </div>
     </div>
   );
