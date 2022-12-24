b0276562112ffaac996af599a5f84a8e8c04e4d5
NadyaCodes <nadyacorscadden@gmail.com>
2022-12-24 00:03:23 -0800

    make frog appear on click

--- src/Grid.js
+++ src/Grid.js
@@ -1,12 +1,18 @@
 import React from "react";
 import frog from "./frog-face-cute.png";
+import { useState } from "react";
 
 export default function Grid() {
+  const [visible, setVisible] = useState(false);
   return (
     <div className="grid">
-      <div className="box star-hide"></div>
-      <div className="frog">
-        <img src={frog} alt="cute frog with bow" />
+      <div className="box star-hide" onClick={() => setVisible(true)}></div>
+      <div>
+        <img
+          src={frog}
+          alt="cute frog with bow"
+          className={visible === true ? "frog visible" : "hidden"}
+        />
       </div>
     </div>
   );
