8953be7609f994a3110e7e65271287cf431e48f4
NadyaCodes <nadyacorscadden@gmail.com>
2022-12-23 23:46:02 -0800

    make frog component

--- src/Grid.js
+++ src/Grid.js
@@ -1,9 +1,13 @@
 import React from "react";
+import frog from "./frog-face-cute.png";
 
 export default function Grid() {
   return (
     <div className="grid">
-      <div className="box"></div>
+      <div className="box star-hide"></div>
+      <div className="frog">
+        <img src={frog} alt="cute frog with bow" />
+      </div>
     </div>
   );
 }
